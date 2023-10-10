fb8246aa72c97bea0870265b6f02fe07b62550f0
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-10-10 11:14:24 -0500

    c

--- main.cpp
+++ main.cpp
@@ -105,7 +105,7 @@ int main() {
                 {
                 string nombreArchivo = separarEspacio(respuesta, true);
                 std::string codigoCodificado = arbolHuffman.codificar(nombreArchivo);
-                crearArchivo(codigoCodificado);
+                crearArchivo(nombreArchivo, codigoCodificado);
                    
                 }
                 break;
@@ -159,15 +159,14 @@ int main() {
 
 //permite crear un archivo
 
-void crearArchivo(const string& codigoCodificado) {
-    ofstream archivo( codigoCodificado);
+void crearArchivo(const string& nombreArchivo, const string& codigoCodificado) {
+    ofstream archivo(nombreArchivo + "_codificado.txt"); // Agregamos "_codificado.txt" al nombre del archivo
     if (archivo.is_open()) {
-
-        cout << "La partida ha sido guardada correctamente "<<endl;
+        archivo << codigoCodificado; // Escribimos el código codificado en el archivo
         archivo.close();
-
+        cout << "La partida ha sido guardada correctamente en " << nombreArchivo << "_codificado.txt." << endl;
     } else {
-        cout << "La partida no ha sido guardada correctamente.\n";
+        cout << "La partida no ha sido guardada correctamente." << endl;
     }
 }
 
